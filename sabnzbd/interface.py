b0a8e70ae5dd893ce4cf60fb99f7a55c424116a6
Safihre <safihre@sabnzbd.org>
2016-01-15 11:17:04 +0100

    Adding MultiPar interpreter

--- sabnzbd/interface.py
+++ sabnzbd/interface.py
@@ -1414,7 +1414,7 @@ class ConfigFolders(object):
 
 ##############################################################################
 SWITCH_LIST = \
-    ('par2_multicore', 'par_option', 'overwrite_files', 'top_only', 'ssl_ciphers',
+    ('par2_multicore', 'multipar', 'par_option', 'overwrite_files', 'top_only', 'ssl_ciphers',
              'auto_sort', 'propagation_delay', 'auto_disconnect', 'flat_unpack',
              'safe_postproc', 'no_dupes', 'replace_spaces', 'replace_dots', 'replace_illegal',
              'ignore_samples', 'pause_on_post_processing', 'quick_check', 'nice', 'ionice',
